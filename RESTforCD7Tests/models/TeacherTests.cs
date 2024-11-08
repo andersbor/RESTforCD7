b86290885b297462c31b0f7b414f95a8acdf3772
andersbor <anbo@zealand.dk>
2024-11-08 21:11:23 +0100

    Teacher salary test

---
+++ RESTforCD7Tests/models/TeacherTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTforCD7.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTforCD7.models.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void ValidateSalaryTest()
+        {
+            Teacher teacherSalaryPositive = new() { Salary = 1 };
+            Teacher teacherSalaryZero = new() { Salary = 0 };
+            Teacher teacherSalaryNegative = new() { Salary = -1 };
+            teacherSalaryPositive.ValidateSalary();
+            teacherSalaryZero.ValidateSalary();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => teacherSalaryNegative.ValidateSalary());
+        }
+    }
+}

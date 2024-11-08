9e14e502bdb31540079d216a52e8c9d815864e7e
andersbor <anbo@zealand.dk>
2024-11-08 21:52:26 +0100

    repo test

---
+++ RESTforCD7Tests/repositories/TeachersRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTforCD7.repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTforCD7.repositories.Tests
+{
+    [TestClass()]
+    public class TeachersRepositoryTests
+    {
+        [TestMethod()]
+        public void TeachersRepositoryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
